2f7bc7536edcd2f1c91c8c51fb8eab33145a69d4
Jimena Armijos <jimenaarmijos@Jimenas-MacBook-Pro.local>
2016-06-28 12:35:59 -0700

    Assign1.js

---
+++ Assign1.js
@@ -0,0 +1,189 @@
+/*
+//STEP 1
+var someMonth;
+function currentMonth(){}
+const CURRENT_MONTH = 10;
+var aMonths = ["Jan","Feb","Mar","Apr","May","jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+function myLibaryAFunction(){}
+*/
+
+/*
+//STEP 2
+    29.80
+    "My name is Jimena"
+    true
+    null
+*/
+
+/*
+//STEP 3
+var totAmount = 43 + 12.18;
+var myName = "Sally" + "Smith";
+*/
+
+/*
+//STEP 4
+var strFirstName, strLastName, strAddress, strCity, strState, strZipCode, intYourAge, //strReferalSource, boolMayWeContactYou
+*/
+
+/*
+//STEP 5
+//hungarian notation
+strFirstName = "Bill"; strLastName = "Smith"; strAddress = "89 Oakly Ave"; strCity = "SD"; strZipCode = "92114";
+numYourAge = 49;  strReferalSource = "newspaper"; boolMayWeContactYou = true;
+//assignment declaration
+var num1 = 22;
+//assignment after declaration
+num1 = 456;
+//assign multiple variables
+num1 = num2 = 34;
+*/  
+
+/*
+//STEP 6
+var var1 = 64 + "48";
+window.console.log(var1);
+            
+var var2 = true + "false";
+window.console.log(var2);
+            
+var var3 = 86 + false;
+window.console.log(var3);
+*/
+
+/*
+//STEP 7
+//hoisting - moves declaration above assignment
+num1 = 27;
+var num1;
+*/
+
+/*
+//STEP 8
+var someString = "Who once said, \"Only two things are infinite, the universe and human stupidity, I\'m not sure about the former.";
+*/
+
+/*
+//STEP 9
+var nullValue = "";
+window.console.log(nullValue);           
+var undefValue;
+window.console.log(undefValue);
+*/
+
+/*
+//STEP 10
+var strVariable = "string";
+var numVariable = 34.56;
+var boolVariable = true;
+var objVariable = new Object;
+var undefVariable = "";
+            
+window.console.log(typeof strVariable);
+window.console.log(typeof numVariable);
+window.console.log(typeof boolVariable);
+window.console.log(typeof objVariable);
+window.console.log(typeof undefVariable);
+*/
+
+/*
+//STEP 11
+alert("Hello " + "Zak Ruvalcaba" + " welcome to the javascript class");
+*/
+
+/*
+//STEP 12
+var strMyName = "Jimena";
+alert("Hello " + strMyName + " welcome to the javascript class");
+*/            
+
+/*
+//STEP 13
+var strCourse = "javascript";
+alert("Hello " + strMyName + " welcome to the " + strCourse + " class");
+*/
+
+/*
+//STEP 14
+alert("Hello " + strMyName + "<br>Welcome to the " + strCourse + " class");
+*/
+
+/*
+//STEP 15
+var strName = window.prompt("Enter you name:");
+alert("Hello " + strName + "<br>Welcome to the " + strCourse + " class");
+*/
+
+/*
+//STEP 16
+var strCourse = window.prompt("Enter you course:");
+alert("Hello " + strName + "<br>Welcome to the " + strCourse + " class"); 
+*/
+
+/*
+//STEP 17
+var x = 10;
+var y = 20;
+window.console.log(x + y);
+*/
+
+/*
+//STEP 18
+var x = 20;
+x += 20;
+window.console.log(x);
+*/
+
+/*
+//STEP 19
+var x = 20;
+x *= 5;
+window.console.log(x);
+*/
+
+/*
+//STEP 20
+var x = 20 % 3;
+x /= 1;
+window.console.log(x);
+*/
+
+/*
+//STEP 21
+var x = (19 > 5 && 16 > 4);
+windows.console.log(x);
+*/
+
+/*
+//STEP 22
+var x = (4 > 11 || !(9 > 3));
+window.console.log(x);
+*/
+
+/*
+//STEP 23
+var oWidget = new Object;
+windows.console.log(typeof oWidget);
+*/
+
+/*
+//STEP 24
+var oWidget = new Object' 
+var boolResult = (oWideget instanceof object);
+if (boolResult){
+    window.console.log("oWidget is an object");
+} else {
+    window.console.log("oWidget is not an object");
+}
+*/
+
+/*
+//STEP 25
+var oWidget = new Object;
+var boolResult = !(oWidget instanceof object);
+if (boolResult){
+    window.console.log("oWidget is not an object");
+} else {
+    window.console.log("oWidget is an object");
+}
+*/
